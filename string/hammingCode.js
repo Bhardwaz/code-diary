f06aa20af4b2f8cc442c78f81835e66fc8ce4f15
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-10-14 13:13:40 +0530

    solution

---
+++ string/hammingCode.js
@@ -0,0 +1,13 @@
+  function hammingDistance(x, y) {
+        let xor = x ^ y;
+        let distance = 0;
+      
+        while (xor > 0) {
+          if (xor & 1) {
+            distance++;
+          }
+          xor >>= 1;
+        }
+      
+        return distance;
+}
